0cca07293c3d573fb9fbbc20bd91c67a024e4c96
Jakub Bujak <kuba.bujak0@wp.pl>
2023-02-13 11:20:08 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navBarLinks = document.getElementsByClassName('navbar__links')[0]
+
+toggleButton.addEventListener('click', () => {
+	navBarLinks.classList.toggle('active');
+
+})
